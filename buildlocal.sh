8f5a01f752e217d000594a57832860d081ecb885
Temani <temani@keysysconsulting.com>
2019-06-03 10:27:26 -0500

    Updated wercker.yml file

--- buildlocal.sh
+++ buildlocal.sh
@@ -1,5 +1,3 @@
 rm -rf _builds _steps _projects
-wercker build --git-domain github.com \
-  --git-owner temanibeck \
-  --git-repository Hello-World-Docker-Wercker
+wercker build --git-domain github.com --git-owner temanibeck --git-repository Hello-World-Docker-Wercker
 rm -rf _builds _steps _projects
